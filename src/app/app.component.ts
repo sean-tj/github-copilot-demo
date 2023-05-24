7a81986c2e0c6f2d381644adc776136087b1e451
kingsnow129 <kingsnow129@gmail.com>
2023-05-24 23:16:12 +0800

    CHG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'github-copilot-demo';
+
+  //validate chinese phone number function
+  validatePhone(phone: string): boolean {
+    const reg = /^1[3456789]\d{9}$/;
+    return reg.test(phone);
+  }
+
+  //validate swedish phone number function
+  validateSwedishPhone(phone: string): boolean {
+    const reg = /^07[02369]\d{7}$/;
+    return reg.test(phone);
+  }
+
+  //validate chinese currency function
+  validateCurrency(currency: string): boolean {
+    const reg = /^¥[0-9]+(.[0-9]{1,2})?$/;
+    return reg.test(currency);
+  }
+
+  //validate french currency function
+  validateFrenchCurrency(currency: string): boolean {
+    const reg = /^[0-9]+(,[0-9]{1,2})?€$/;
+    return reg.test(currency);
+  }
 }
