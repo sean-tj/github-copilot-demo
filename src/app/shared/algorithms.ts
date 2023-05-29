661985870dc5873c8875a87807fb138ed66da737
Sean Wang <Sean.Wang@volvo.com>
2023-05-29 10:00:35 +0800

    Modify title

---
+++ src/app/shared/algorithms.ts
@@ -0,0 +1,26 @@
+export function getString() {
+    return [
+        'en', {
+            'home': 'Home',
+            'about': 'About',
+            'contact': 'Contact',
+            'login': 'Login',
+            'register': 'Register',
+            'logout': 'Logout',
+            'profile': 'Profile',
+            'dashboard': 'Dashboard',
+            'welcome': 'Welcome'
+        },
+        'vi', {
+            'home': 'Trang chủ',
+            'about': 'Giới thiệu',
+            'contact': 'Liên hệ',
+            'login': 'Đăng nhập',
+            'register': 'Đăng ký',
+            'logout': 'Đăng xuất',
+            'profile': 'Hồ sơ',
+            'dashboard': 'Bảng điều khiển',
+            'welcome': 'Chào mừng'
+        }
+    ]
+}
