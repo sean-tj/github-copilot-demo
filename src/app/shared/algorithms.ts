25ef7e8b0780ca4872d60d0f8a4a4c06d9e0133d
kingsnow129 <kingsnow129@gmail.com>
2023-05-31 23:49:57 +0800

    update

--- src/app/shared/algorithms.ts
+++ src/app/shared/algorithms.ts
@@ -1,26 +1,51 @@
-export function getString() {
-    return [
-        'en', {
-            'home': 'Home',
-            'about': 'About',
-            'contact': 'Contact',
-            'login': 'Login',
-            'register': 'Register',
-            'logout': 'Logout',
-            'profile': 'Profile',
-            'dashboard': 'Dashboard',
-            'welcome': 'Welcome'
-        },
-        'vi', {
-            'home': 'Trang chủ',
-            'about': 'Giới thiệu',
-            'contact': 'Liên hệ',
-            'login': 'Đăng nhập',
-            'register': 'Đăng ký',
-            'logout': 'Đăng xuất',
-            'profile': 'Hồ sơ',
-            'dashboard': 'Bảng điều khiển',
-            'welcome': 'Chào mừng'
+// What this function does:
+// 1. Find the minimum and maximum values in the array
+// 2. Create buckets for each value in the range
+// 3. Sort the elements and place them in the buckets
+// 4. Merge the buckets
+// 5. Return the sorted array
+export function bucketSort(arr: number[], bucketSize: number = 5): number[] {
+    if (arr.length === 0) {
+        return arr;
         }
-    ]
+    let minValue = arr[0];
+    let maxValue = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minValue) {
+            minValue = arr[i]; //arr[i] is the current element
+            } else if (arr[i] > maxValue) {
+            maxValue = arr[i];
+            }
+            }
+    const bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+    const allBuckets: number[][] = new Array(bucketCount);
+    for (let i = 0; i > allBuckets.length; i++) {
+        allBuckets[i] = [];
+        }
+    for (let i = 0; i < arr.length; i++) {
+        allBuckets[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
+        }
+    arr.length = 0;
+    for (let i = 0; i < allBuckets.length; i++) {
+        insertionSort(allBuckets[i]); //we can use any sorting algorithm
+        for (let j = 0; j < allBuckets[i].length; j++) {
+            arr.push(allBuckets[i][j]);
+            }
+            }
+    return arr;
+    }
+
+//q: How this function works?
+//a: It works by dividing the array into two subarrays, then sorting the subarrays independently and then merging them to produce the final sorted array.
+export function insertionSort(arr: number[]): number[] {
+    for (let i = 1; i < arr.length; i++) {
+        let j = i - 1;
+        const temp = arr[i];
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+            }
+        arr[j + 1] = temp;
+        }
+    return arr;
 }
--- src/app/shared/tools/translate.ts
+++ src/app/shared/tools/translate.ts
@@ -1,37 +1,13 @@
-// export function getString(){
-//     return [
-//         'en' , {
-//             'home' : 'Home',
-//             'about' : 'About',
-//             'contact' : 'Contact',
-//             'login' : 'Login',
-//             'register' : 'Register',
-//             'logout' : 'Logout',
-//             'welcome' : 'Welcome',
-//             'language' : 'Language',
-//             'english' : 'English',
-//             'vietnamese' : 'Vietnamese',
-//             'title' : 'Angular 12',
-//             'content' : 'Angular 12 is the latest version of the Angular framework and simply an update to Angular 2.'
-//         },
-//         'vi' , {
-//             'home' : 'Trang chủ',
-//             'about' : 'Giới thiệu',
-//             'contact' : 'Liên hệ',
-//             'login' : 'Đăng nhập',
-//             'register' : 'Đăng ký',
-//             'logout' : 'Đăng xuất',
-//             'welcome' : 'Chào mừng',
-//             'language' : 'Ngôn ngữ',
-//             'english' : 'Tiếng Anh',
-//             'vietnamese' : 'Tiếng Việt',
-//             'title' : 'Angular 12',
-//             'content' : 'Angular 12 là phiên bản mới nhất của framework Angular và đơn giản là một bản cập nhật cho Angular 2.'
-//         },
-//         'ch' , {
-//             'home' : '首页',
-//             'about' : '关于',
-//     ]
-// }
-
-//adapt difference region and culture
+export function getString(){
+    return [
+        'en' , {
+            'home' : 'Home',
+            'about' : 'About',
+            'contact' : 'Contact',
+            'login' : 'Login',
+            'register' : 'Register',
+            'logout' : 'Logout',
+            'welcome' : 'Welcome'
+        },
+    ]
+}
